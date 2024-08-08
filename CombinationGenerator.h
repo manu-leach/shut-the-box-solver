2fda2555b7ef8a00a1f8acc976b18a242cc5a9b4
Manu Leach <manuleach02@gmail.com>
2024-08-08 22:14:50 +0100

    static combo gen

--- CombinationGenerator.h
+++ CombinationGenerator.h
@@ -8,10 +8,10 @@ class CombinationGenerator
 public:
     CombinationGenerator() {}
 
-    std::set<std::set<int>> genCombos(const int& total);
+    static std::set<std::set<int>> genCombos(const int& total);
 
 private:
-    void genCombosRecursive(std::set<std::set<int>>& combos, std::set<int>& currentCombo, std::set<int>& numbersUp, const int& total);
+    static void genCombosRecursive(std::set<std::set<int>>& combos, std::set<int>& currentCombo, std::set<int>& numbersUp, const int& total);
 };
 
 #endif
